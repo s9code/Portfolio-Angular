b8ea804f2d34a809fccddecda93347d1d7aa6cb9
s9code <shotarocode@gmail.com>
2024-09-08 13:53:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true, // Indica que es un componente independiente
+  imports: [RouterOutlet, HomeComponent, ProjectsComponent, ContactComponent], // Se importa RouterOutlet para gestionar las rutas
+  templateUrl: './app.component.html', 
+  styleUrl: './app.component.css' // Usa los estilos correspondientes
 })
 export class AppComponent {
   title = 'portfolio-angular';
